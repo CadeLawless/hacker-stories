7b4ea7bda9ce83d23e8afde73ad3b7d356761eb5
Cade Lawless <clawless05@gmail.com>
2025-01-03 17:03:55 -0600

    Split up code into new components

--- src/App.jsx
+++ src/App.jsx
@@ -24,17 +24,32 @@ function App() {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor='search'>Search: </label>
-      <input id='search' type='text' />
+      <Search />
 
       <hr />
 
-      <ul>
+      <List />
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
         {list.map(function (item) {
           return (
             <li key={item.objectID}>
               <span>
-                <a href={item.url}>{item.title}</a>
+                <a target='_blank' href={item.url}>{item.title}</a>
               </span>
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
@@ -43,7 +58,6 @@ function App() {
           );
         })}
       </ul>
-    </div>
   );
 }
 
