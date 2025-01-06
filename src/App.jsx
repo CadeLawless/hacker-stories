80175d17668a662d3d401269d939616a3cb39640
Cade Lawless <clawless05@gmail.com>
2025-01-06 14:17:06 -0600

    Added a useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -40,16 +40,35 @@ const App = () => {
       }, 2000)
     );
 
+  const storiesReducer = (state, action) => {
+    switch (action.type) {
+      case 'SET_STORIES':
+        return action.payload;
+      case 'REMOVE_STORY':
+        return state.filter(
+          (story) => action.payload.objectID !== story.objectID
+        );
+      default:
+        throw new Error('Invalid action type');
+    }
+  };
+
   const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
-  const [stories, setStories] = React.useState([]);
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer,
+    []
+  );
   const [isLoading, setIsLoading] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
     setIsLoading(true);
     getAsnycStories().then(result => {
-      setStories(result.data.stories);
+      dispatchStories({
+        type: 'SET_STORIES',
+        payload: result.data.stories,
+      });
       setIsLoading(false);
     })
     .catch(() => setIsError(true));
@@ -60,10 +79,10 @@ const App = () => {
   };
 
   const handleRemoveStory = (item) => {
-    const newStories = stories.filter(
-      (story) => item.objectID !== story.objectID
-    );
-    setStories(newStories);
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    });
   };
 
   const searchedStories = stories.filter((story) =>
