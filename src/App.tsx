b9685432803d1777cdd202db75e7288285d05b7f
Cade Lawless <clawless05@gmail.com>
2025-01-15 17:18:20 -0600

    Integrated TypeScript into the app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,96 @@ import axios from 'axios';
 import './App.css';
 import Check from './check.svg?react';
 
-const useStorageState = (key, initialState) => {
+type Story = {
+  objectID: string;
+  url: string;
+  title: string;
+  author: string;
+  num_comments: number;
+  points: number;
+};
+
+type Stories = Story[];
+
+type ItemProps = {
+  item: Story;
+  onRemoveItem: (item: Story) => void;
+}
+
+type ListProps = {
+  list: Stories;
+  onRemoveItem: (item: Story) => void;
+}
+
+type StoriesState = {
+  data: Stories;
+  isLoading: boolean;
+  isError: boolean;
+};
+
+type StoriesFetchInitAction = {
+  type: 'STORIES_FETCH_INIT';
+};
+
+type StoriesFetchSuccessAction = {
+  type: 'STORIES_FETCH_SUCCESS';
+};
+
+type StoriesFetchFailureAction = {
+  type: 'STORIES_FETCH_FAILURE';
+};  
+
+type StoriesRemoveAction = {
+  type: 'REMOVE_STORY';
+  payload: Story;
+};
+
+type StoriesAction = 
+  StoriesFetchInitAction
+  | StoriesFetchSuccessAction
+  | StoriesFetchFailureAction
+  | StoriesRemoveAction;
+
+const storiesReducer = (
+  state: StoriesState,
+  action: StoriesAction,
+) => {
+  switch (action.type) {
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case 'REMOVE_STORY':
+      return {
+        ...state,
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        ),
+      };
+    default:
+      throw new Error('Invalid action type');
+  }
+};
+
+const useStorageState = (
+  key: string,
+  initialState: string
+): [string, (newValue: string) => void] => {
   const isMounted = React.useRef(false);
 
   const [value, setValue] = React.useState(
@@ -24,6 +113,21 @@ const useStorageState = (key, initialState) => {
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
+type SearchFormProps = {
+  searchTerm: string;
+  onSearchInput: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onSearchSubmit: (event: React.FormEvent<HTMLInputElement>) => void;
+};
+
+type InputWithLabelProps = {
+  id: string;
+  value: string;
+  type?: string;
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  isFocused?: boolean;
+  children: React.ReactNode;
+}
+
 const getSumComments = (stories) => {
   console.log('C');
   return stories.data.reduce(
@@ -33,38 +137,6 @@ const getSumComments = (stories) => {
 };
 
 const App = () => {
-  const storiesReducer = (state, action) => {
-    switch (action.type) {
-      case 'STORIES_FETCH_INIT':
-        return {
-          ...state,
-          isLoading: true,
-          isError: false,
-        };
-      case 'STORIES_FETCH_SUCCESS':
-        return {
-          ...state,
-          isLoading: false,
-          isError: false,
-          data: action.payload,
-        };
-      case 'STORIES_FETCH_FAILURE':
-        return {
-          ...state,
-          isLoading: false,
-          isError: true,
-        };
-      case 'REMOVE_STORY':
-        return {
-          ...state,
-          data: state.data.filter(
-            (story) => action.payload.objectID !== story.objectID
-          ),
-        };
-      default:
-        throw new Error('Invalid action type');
-    }
-  };
 
   const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
@@ -97,21 +169,25 @@ const App = () => {
     handleFetchStories();
   }, [handleFetchStories]);
 
-  const handleSearchInput = (event) => {
+  const handleSearchInput = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setSearchTerm(event.target.value);
   };
-  const handleSearchSubmit = (event) => {
+  const handleSearchSubmit = (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
 
     event.preventDefault();
   };
 
-  const handleRemoveStory = React.useCallback((item) => {
+  const handleRemoveStory = (item: Story) => {
     dispatchStories({
       type: 'REMOVE_STORY',
       payload: item,
     });
-  }, []);
+  };
 
   console.log('B:App');
 
@@ -141,7 +217,7 @@ const App = () => {
   );
 }
 
-const SearchForm = ({
+const SearchForm: React.FC<SearchFormProps> = ({
   searchTerm,
   onSearchInput,
   onSearchSubmit,
@@ -164,7 +240,7 @@ const SearchForm = ({
   </form>
 );
 
-const InputWithLabel = ({
+const InputWithLabel: React.FC<InputWithLabelProps> = ({
   id,
   value,
   type = 'text',
@@ -172,7 +248,7 @@ const InputWithLabel = ({
   isFocused,
   children,
 }) => {
-  const inputRef = React.useRef();
+  const inputRef = React.useRef<HTMLInputElement>();
 
   React.useEffect(() => {
     if (isFocused && inputRef.current) {
@@ -195,19 +271,17 @@ const InputWithLabel = ({
   );
 };
 
-const List = React.memo(
-  ({ list, onRemoveItem }) => 
-    console.log('B:List') || (
-    <div>
-      <ul>
-        {list.map((item) => (
-            <Item onRemoveItem={onRemoveItem} key={item.objectID} item={item} />
-          ))}
-      </ul>
-    </div>
-  )
+const List: React.FC<ListProps> = ({ list, onRemoveItem }) => (
+  <div>
+    <ul>
+      {list.map((item) => (
+          <Item onRemoveItem={onRemoveItem} key={item.objectID} item={item} />
+        ))}
+    </ul>
+  </div>
 );
-const Item = ({ item, onRemoveItem }) => (
+
+const Item: React.FC<ItemProps> = ({ item, onRemoveItem }) => (
   <li className='item' key={item.objectID}>
     <span style={{ width: '40%' }}>
       <a target='_blank' href={item.url}>{item.title}</a>
