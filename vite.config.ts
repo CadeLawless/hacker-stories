f4e8677b791c5cddb7ce56ead1ac2d3d903b788b
Cade Lawless <clawless05@gmail.com>
2025-01-16 16:44:54 -0600

    Did some component testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { configDefaults } from 'vitest/config'; // Import Vitest configuration defaults
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts',
+  },
 });
